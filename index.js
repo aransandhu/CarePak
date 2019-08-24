4125f7085d853516b7368271ce54ed3922884c8c
aran <aransandhu97@yahoo.ca>
2019-08-24 17:37:30 -0400

    progress

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const iplocation = require("iplocation").default;
+var ip = require('ip');
+
 const mongooseDSN = 'mongodb://localhost:27017/carepak';
 const UserModel = require('./model/user');
 const PackageModel = require('./model/package');
@@ -25,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // signup route - create user
 app.post('/user/signup', async (req, res, next) => {
     try {
-        console.log(req.body)
+        console.log(req.body);
         var user = new UserModel({
             username: req.body.username,
             password: req.body.password,
@@ -34,7 +37,7 @@ app.post('/user/signup', async (req, res, next) => {
             email: req.body.email,
             phone: req.body.phone,
             icon: req.body.icon,
-            history: [],
+            history: []
         });
         const savedUser = await user.save();
         return res.json(savedUser);
@@ -52,19 +55,19 @@ app.get('/all_packages', async (req, res, next) => {
     } catch(err) {
         return next(err);
     }
-})
+});
 
 app.get('/create_package', async (req, res, next) => {
     try {
         var newPackage = new PackageModel({
-            items: [],
+            items: []
         });
         const savedPackage = await newPackage.save();
         return res.json(savedPackage);
     } catch(err) {
         return next(err);
     }
-})
+});
 
 app.post('/add_item_to_package', async (req, res, next) => {
     try {
@@ -76,8 +79,9 @@ app.post('/add_item_to_package', async (req, res, next) => {
             name: req.body.name,
             price: req.body.price,
             picture: req.body.picture,
-            category: req.body.category,
+            category: req.body.category
         });
+<<<<<<< HEAD
         const savedItem = await newItem.save();
         await PackageModel.update(
             {_id: mongoose.Types.ObjectId(req.body.packageId)},
@@ -85,10 +89,26 @@ app.post('/add_item_to_package', async (req, res, next) => {
         );
         const updatedPackage = await PackageModel.findById(req.body.packageId);
         return res.json(`New Item ${JSON.stringify(savedItem)} added to package ${JSON.stringify(updatedPackage)}`);
+=======
+        let savedItem = await item.save();
+        savedItem.save(function (err, item) {
+            if (err) return console.error(err);
+            console.log(item.name);
+          });
+
+
+        // await PackageModel.update(
+        //     {_id: req.params.packageId},
+        //     {$push: {items: savedItem._id, 0}}
+        // );
+
+        const updatedPackage = await PackageModel.findById(req.packageId);
+        return res.json('New Item ${JSON.stringify(savedItem)} added to package ${JSON.stringify(updatedPackage)}');
+>>>>>>> progress
     } catch(err) {
         return next(err);
     }
-})
+});
 
 app.post('/donate', async (req, res, next) => {
     // create donation object
@@ -99,8 +119,8 @@ app.post('/donate', async (req, res, next) => {
             user: req.body.userId,
             package: req.body.packageId,
             item: req.body.itemId,
-            amount: req.body.amount,
-        })
+            amount: req.body.amount
+        });
         const savedDonation = await donation.save();
         await UserModel.update(
             {_id: req.body.userId},
@@ -136,7 +156,54 @@ app.post('/donate', async (req, res, next) => {
     } catch(err) {
         next(err);
     }
-})
+});
+
+app.get('/popular/:location', async (req, res, next) => {
+    var location = req.params.location;
+
+    try {
+      if(location == 'worldwide'){
+        var packages = PackageModel.find({}).sort('-numDonations').exec(function(err, package) {
+            res.json(package);
+        });
+      }else{
+        var city = 'Toronto';
+        iplocation(req.connection.remoteAddress, [], (error, res) => {
+            if(res.city != ''){
+                city = res.city;
+            }
+            console.log(city);
+        });
+      }
+    } catch(err) {
+        next(err);
+    }
+});
+
+app.get('/category/:category', async (req, res, next) => {
+    var category = req.params.category;
+
+    ItemsModel.find({category: category}, function(err, docs){
+        var itemIds = docs.map(function(doc) { return doc._id; });
+
+
+        PackageModel.find({items.item: {$in: itemIds}}, function(err, docs) {
+            console.log(docs);
+        });
+    });
+
+]
+
+    try {
+
+        PackageModel.find({'items.item.category': category}).exec(function(err, package) {
+            r
+        });
+    } catch(err) {
+        next(err);
+    }
+
+});
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
@@ -153,4 +220,4 @@ mongoose.connect(mongooseDSN, {
     console.log(`connected to mongodb`);
     app.listen(port);
     console.log('App is listening on port ' + port);
-  })
+  });
--- model/package.js
+++ model/package.js
@@ -3,12 +3,20 @@ var Schema = mongoose.Schema;
 
 var PackageSchema = new Schema({
   items: [{
+<<<<<<< HEAD
       item: { type: Schema.Types.ObjectId,
       ref: 'Item',
       }, 
       amountPaid: Number,
       numDonations: Number,
+=======
+      item: {
+        type: Schema.Types.ObjectId,
+        ref: 'Item'},
+      amountPaid: Number,
+      numDonations: Number
+>>>>>>> progress
   }]
-})
+});
 
 module.exports = mongoose.model('Package', PackageSchema);
