564e876c910671c98168789d1d22bb618632dad8
Michelle <michelle.gong@uwaterloo.ca>
2019-08-24 01:29:58 -0400

    install matrial ui

---
+++ client/src/Profile/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        hello! How are you?
+      </div>
+    );
+  }
+}
+
+export default Profile;
