08999c3c5a404d5da018e5721e320d938e96d008
Julian Zhang <19332422+JulianZhang97@users.noreply.github.com>
2019-08-24 11:17:14 -0400

    Gallery and item in progress

--- client/src/Gallery/Home.js
+++ client/src/Gallery/Home.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import styled from 'styled-components';
 
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
 
 import Package from './Package/Package';
 
+import SimpleMenu from './components/SimpleMenu'
+
+
 class Home extends Component {
+  state = {
+    selection : 1
+  };
+
+  changeFilter(filterID){
+    this.setState({selection: filterID}, () => {console.log(filterID)})
+  }
+
+
   render() {
     return (
     <div className="App">
@@ -18,15 +30,22 @@ class Home extends Component {
             My List
         </button>
       </Link>
+      <SimpleMenu 
+        changeFilter={this.changeFilter}
+      >
+        <MenuItem value={1} primaryText="English"  />
+        <MenuItem value={2} primaryText="Spanish" />
+        <MenuItem value={3} primaryText="French" />
+      </SimpleMenu>
 
       <Grid container spacing={3}>
-        <Package/>
-        <Package/>
-        <Package/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
 
-        <Package/>
-        <Package/>
-        <Package/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
+        <Package packageItem={{title: "Local Charity Box"}}/>
       </Grid>
     </div>
     );
--- client/src/Gallery/Package/Package.js
+++ client/src/Gallery/Package/Package.js
@@ -15,40 +15,25 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-class Package extends Component {
-  render() {
-    return (
-        <Grid container item xs={4}>
-          <Card>
+const Package = ({ packageItem }) => (
+    <Grid item item xs={4}>
+        <Card>
             <CardActionArea>
-              {/* <CardMedia
-                image="/closed_box.png"
-                title="Contemplative Reptile"
-              /> */}
-              <Box/>
-              <CardContent>
+                <Box/>
+                <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
+                    {packageItem.title}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
                 </Typography>
-              </CardContent>
+                </CardContent>
             </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-    );
-  }
-}
+        </Card>
+    </Grid>
+);
+
 export default Package;
 
 const Box = styled.img.attrs({
