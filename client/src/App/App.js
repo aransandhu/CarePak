aed539f3eb73a8d33694d8ae63a01655bc11764b
Michelle <michelle.gong@uwaterloo.ca>
2019-08-24 09:24:21 -0400

    changes

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -28,24 +28,91 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from '../Gallery/Home';
-import List from '../Gallery/List';
+// import Home from '../Gallery/Home';
+// import List from '../Gallery/List';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  FormGroup,
+  FormControlLabel
+} from '@material-ui/core';
+import { AccountCircle, Menu as MenuIcon } from '@material-ui/icons';
 
 class App extends Component {
+  state = {
+    auth: true,
+    setAuth: true,
+    anchorEl: false,
+    setAnchorEl: false,
+    open: false,
+  }
+
+  handleChange = (e) => {
+    this.setState({ auth: e.target.checked })
+  }
+
+  handleMenu = (e) => {
+    this.setState({ setAnchorEl: e.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ setAnchorEl: null })
+  }
+
   render() {
-    const App = () => (
+    const { auth, anchorEl, setAnchorEl, open } = this.state;
+
+    return (
       <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
-        </Switch>
+        <AppBar position="static" style={{ background: '#303C6C' }}>
+          <Toolbar>
+            <IconButton edge="start" style={{ marginRight: '1rem' }} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              CarePak
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                  edge="end"
+                  style={{marginLeft: '85vw'}}
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
     )
-    return (
-      <Switch>
-        <App/>
-      </Switch>
-    );
   }
 }
 
--- client/src/Profile/index.js
+++ client/src/Profile/index.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Info } from './components'
+import { Grid, Typography, IconButton } from '@material-ui/core';
+import Icon from '@mdi/react'
+import { mdiFacebook, mdiTwitter } from '@mdi/js';
 
 class Profile extends Component {
+
   render() {
+    const user = {
+      firstName: 'Julian',
+      lastName: 'Zhang',
+      user: 'jay-zee'
+    };
+
     return (
-      <div style={{ padding: '5rem' }}>
-        <Info
-          height='10rem'
-          width='15rem'
-          header="nihaoma"
-          icon="yes"
-        />
-        First Last
-        username
-        icons for social media
-
-        User tags
-
-
-        Purchase history
+      <div style={{ padding: '1rem 5rem' }}>
+        <Grid container spacing={5}>
+          <Grid item xs={4}>
+            <Info
+              height='28vh'
+              width='100%'
+              header="Basic Info"
+              icon="yes"
+              content={
+                <div style={{ marginTop: '1rem' }}>
+                  <Typography variant='h6' style={{ fontWeight: '700' }}>{user.firstName} {user.lastName}</Typography>
+                  <Typography variant='subtitle1'>{user.user}</Typography>
+                  <IconButton
+                    style={{ background: 'lightblue'}}
+                    size="small"
+                  >
+                    <Icon path={mdiTwitter}
+                      title="User Profile"
+                      size={1}
+                      horizontal
+                      vertical
+                      rotate={90}
+                      color="white"
+                      spin
+                    />
+                  </IconButton>
+                  <IconButton
+                    style={{ background: 'blue', marginLeft: '1rem'}}
+                    size="small"
+                  >
+                    <Icon path={mdiFacebook}
+                      title="User Profile"
+                      size={1}
+                      horizontal
+                      vertical
+                      rotate={90}
+                      color="white"
+                      spin
+                    />
+                  </IconButton>
+                </div>
+              }
+            />
+            <Info
+              height='28vh'
+              width='100%'
+              header="Interests"
+              content="Nothing here yet!"
+            />
+          </Grid>
+          <Grid item xs={7}>
+            <Info
+              height='75vh'
+              width='100%'
+              header="Donation History"
+              content="Nothing here yet!"
+            />
+          </Grid>
+        </Grid>
       </div>
     );
   }
