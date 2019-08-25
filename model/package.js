eafbcca09de34769b2ab49fa060048dea8a1b8ea
aran <aransandhu97@yahoo.ca>
2019-08-24 21:31:03 -0400

    change to sqlite

--- model/package.js
+++ model/package.js
@@ -3,19 +3,11 @@ var Schema = mongoose.Schema;
 
 var PackageSchema = new Schema({
   items: [{
-<<<<<<< HEAD
       item: { type: Schema.Types.ObjectId,
       ref: 'Item',
-      }, 
+      },
       amountPaid: Number,
       numDonations: Number,
-=======
-      item: {
-        type: Schema.Types.ObjectId,
-        ref: 'Item'},
-      amountPaid: Number,
-      numDonations: Number
->>>>>>> progress
   }]
 });
 
