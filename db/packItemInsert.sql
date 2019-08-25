4cf1e7e1576341dc1787a6eb09a3ab5cecf0e270
aran <aransandhu97@yahoo.ca>
2019-08-25 02:32:27 -0400

    more data

---
+++ db/packItemInsert.sql
@@ -0,0 +1,335 @@
+-- Winter
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 1);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 57);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 58);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 59);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 34);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 11);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 24);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 16);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 1, 60);
+
+-- School
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 39);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 42);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 41);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 38);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 40);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 29);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 43);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 44);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 2, 45);
+
+-- Food
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 7);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 8);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 9);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 10);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 16);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 12);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 13);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 14);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 3, 15);
+
+-- Christmas 
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 49);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 50);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 51);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 52);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 96);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 53);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 54);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 55);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 4, 4);
+
+-- Life Necc #1
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 24);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 25);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 37);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 30);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 31);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 34);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 28);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 32);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 5, 33);
+
+-- Life Necc #2
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 24);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 36);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 30);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 31);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 32);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 35);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 28);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 22);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 6, 68);
+
+-- Sports
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 46);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 47);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 48);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 61);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 62);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 69);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 70);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 67);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 7, 78);
+
+-- Home Package
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 71);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 72);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 74);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 17);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 34);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 75);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 73);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 76);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 8, 77);
+
+-- Kitchen Package
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 18);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 19);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 20);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 21);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 64);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 66);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 65);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 63);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 9, 86);
+
+-- Art Package
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 83);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 84);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 97);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 39);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 40);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 92);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 93);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 94);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 10, 76);
+
+-- Closest Package
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 79);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 80);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 81);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 85);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 86);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 87);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 88);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 89);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 11, 91);
+
+-- Disaster Relief
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 32);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 30);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 24);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 50);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 29);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 13);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 15);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 16);
+
+INSERT INTO PackageItem(paidSoFar, Package_id, Item_id) 
+VALUES (0, 12, 35);
