9fd241d693ccd44324ff8904acb2739bff613f24
Michelle <michelle.gong@uwaterloo.ca>
2019-08-25 00:20:27 -0400

    button

--- client/src/ItemView/ItemView.js
+++ client/src/ItemView/ItemView.js
@@ -107,11 +107,47 @@ right: calc(31% - 140px);
 `
 
 const Back = styled.div `
-  background-image: linear-gradient(to right, #4CB8C4 0%, #3CD3AD 51%, #4CB8C4 100%);
-  height: 5vh;
-  width: 10vw;
+  -webkit-appearance: none;
+  background: -webkit-gradient(to right,#a2ccb6 0%,#fceeb5 50%,#ee786e 100%);
+  background: linear-gradient(to right,#a2ccb6 0%,#fceeb5 50%,#ee786e 100%);
+  background-size: 500%;
+  border: none;
+  border-radius: 2rem;
+  box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
   color: white;
+  cursor: pointer;
+  font-size: 1em;
+  font-weight: 700;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 3rem;
+  letter-spacing: .05em;
+  outline: none;
+  -webkit-tap-highlight-color: transparent;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  width: 12rem;
+  margin-top: 3rem;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%
+    }
+    100% {
+      background-position: 100%
+    }
+  }
+
   &:hover {
-    background-position: right center;
+    animation-name: gradient;
+    -webkit-animation-name: gradient;
+    animation-duration: 2s;
+    -webkit-animation-duration: s;
+    animation-iteration-count: 1;
+    -webkit-animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
   }
 `
